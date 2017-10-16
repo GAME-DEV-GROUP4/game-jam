49889847b822840f3c9e878b716bda0b22a97db1
LEMONBAR\Jennifer <jeilei016@gmail.com>
2017-10-16 10:21:31 -0500

    fixed jump bug

--- Boss/bossLevel.js
+++ Boss/bossLevel.js
@@ -23,9 +23,10 @@ demo.bossLevel.prototype=
     game.load.image('warningdeadline', 'assets/deadline-warning.png');
     game.load.image('deadline', 'assets/deadline.png');
     game.load.spritesheet('bossman', 'assets/boss-man-2-68x120.png', 68, 120);
-    game.load.spritesheet('dude', 'assets/dude.png', 28, 48);
+    game.load.spritesheet('dude', 'assets/big-dude.png', 41, 78);
     game.load.image('floor', 'assets/floor.png');
     game.load.image('background', 'assets/bg.png');
+    game.load.image('fireball', 'assets/projectile.png')
     
     },
     
@@ -42,7 +43,7 @@ demo.bossLevel.prototype=
 
         //floor
         background = game.add.sprite(0, 0, 'background');
-        floor = game.add.sprite(0, 580, 'floor');
+        floor = game.add.sprite(200, 580, 'floor');
         game.physics.arcade.enable(floor);
         floor.body.immovable = true;
         
@@ -57,14 +58,14 @@ demo.bossLevel.prototype=
         bossman.animations.play('idle', 8, true);
         
         //player
-        player = game.add.sprite(100, 500, 'dude');
+        player = game.add.sprite(200, 450, 'dude');
         player.scale.setTo(1.5, 1.5);
         game.physics.enable(player);
-        player.body.gravity.y = 500;
+        player.body.gravity.y = 300;
         player.body.collideWorldBounds = true;
         
-        player.animations.add('left', [0, 1, 0, 1], 5, true);
-        player.animations.add('right', [2, 3, 2, 3], 5, true);
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
         
         //projectiles
         suits = game.add.group();
@@ -75,44 +76,45 @@ demo.bossLevel.prototype=
     },
     
     update: function(){
-        playerMove();
         
         //handle collisions
         var hitBullet = game.physics.arcade.overlap(player, suits, hitSuit, null, this);
         var bossOverlapX = game.physics.arcade.Overlap
         game.physics.arcade.collide(player, floor);
         
+        playerMove();
+        
         if(this.game.time.totalElapsedSeconds()-hitTime <1){
-            playerx = 75;
-            playery = 175;
+            playerx = 200;
+            playery = 300;
         }
         else{
-            playerx = 150;
-            playery=350;
+            playerx = 200;
+            playery = 300;
         }
         
         if(testAttackButton.isDown){
             suitAttack();
         }
         if(testAttackButton2.isDown){
-            deadlinehorizontal(player);
+            predeadlinehorizontal(player);
         }
         if(testAttackButton3.isDown){
-            deadlinevertical(player);
+            predeadlinevertical(player);
         }
     }
 };
 
 function predeadlinehorizontal(player){
     var ypos = player.y + 24;
-    var predeadline = attackSprites.create(0, ypos, 'warningdeadline');
+    var predeadline = attackSprites.create(200, ypos, 'warningdeadline');
     predeadline.width = 800;
     game.add.tween(predeadline).to( { alpha: 0 }, 500, Phaser.Easing.Linear.None, true);
-    game.time.events.add(3000, deadlinehorizontal, this, player, ypos);
+    game.time.events.add(1000, deadlinehorizontal, this, player, ypos);
 }
     
 function deadlinehorizontal(player, ypos){
-    var deadline = attackSprites.create(0, ypos, 'deadline');
+    var deadline = attackSprites.create(200, ypos, 'deadline');
     deadline.width = 800;
     game.add.tween(deadline).to( { alpha: 0 }, 500, Phaser.Easing.Linear.None, true);
 }
@@ -123,7 +125,7 @@ function predeadlinevertical(player){
     predeadline.width = 600;
     predeadline.angle = 90;
     game.add.tween(predeadline).to( { alpha: 0 }, 500, Phaser.Easing.Linear.None, true);
-    game.time.events.add(3000, deadlinevertical, this, player, xpos);
+    game.time.events.add(1000, deadlinevertical, this, player, xpos);
 }
     
 function deadlinevertical(player, xpos){
@@ -134,9 +136,10 @@ function deadlinevertical(player, xpos){
 }
 
 function suitAttack(){
-    var suit = suits.create(bossman.x, bossman.y, 'star');
+    var suit = suits.create(bossman.x, bossman.y, 'fireball');
     game.physics.enable(suit);
-    game.physics.arcade.moveToObject(suit, player);
+    game.physics.arcade.moveToObject(suit, player, 300);
+
 }
 function hitSuit(player, suit)
 {
@@ -148,8 +151,7 @@ function playerMove()
 {
     //set player movement
     player.body.velocity.x = 0;
-    player.body.velocity.y = 0;
-
+    
     if (cursors.left.isDown)
     {
         player.body.velocity.x = 0-playerx;
@@ -167,7 +169,7 @@ function playerMove()
     else 
     {
         player.animations.stop();
-        player.frame = 2;
+        player.frame = 4;
     }
         
         
